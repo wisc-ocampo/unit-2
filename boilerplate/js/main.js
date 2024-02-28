211f56d16fa1264c0441ff38edebe0339d075532
fvocampo <157857305+wisc-ocampo@users.noreply.github.com>
2024-02-27 19:21:09 -0600

    Activity 6.3.2 (NULL SLIDER)

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,70 +1,51 @@
-// Ocampo, Week 5
-//declare map variable globally so all functions have access
+// Ocampo, ACTIVITY 6
+
 var map;
 var minValue;
 
-//step 1 create map
 function createMap(){
 
-    //create the map
     map = L.map('map', {
         center: [0, 0],
         zoom: 2
     });
 
-// 6.1 CHANGE BASE TILELAYER - ESRI WORLD GRAY
-
-    //add OSM base tilelayer
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
 	maxZoom: 16
 }).addTo(map);
 
-    //call getData function
     getData(map);
 };
 
-// 6.1 CHANGE VARIABLE LOOP - year > month
+// 6.1 EDIT, UPDATE year > month
 
 function calculateMinValue(data){
-    //create empty array to store all data values
     var allValues = [];
-    //loop through each city
     for(var city of data.features){
-        //loop through each month
         for(var month = 1; month <= 12; month+=1){
-              //get population for current year
               var value = city.properties["Month_"+ String(month)];
-              //add value to array
               allValues.push(value);
         }
     }
-    //get minimum value of our array
     var minValue = Math.min(...allValues)
-
     return minValue;
 }
 
-//calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
-    //constant factor adjusts symbol sizes evenly
     var minRadius = 5;
-    //Flannery Apperance Compensation formula
     var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
-
     return radius;
 };
 
-// 6.1 SET ATTRIBUTE VARIABLE TO MONTHS ( 1 - 12 )
-// 6.2.1 CHANGE FUNCTION
+// 6.1 EDIT, SET ATTRIBUTE TO MONTHS [ 1 - 12 ]
+// 6.2 EDIT, CHANGE FUNCTION
 
-//Step 3: Add circle markers for point features to the map
 function pointToLayer(feature, latlng){
 
-    //Step 4: Determine which attribute to visualize with proportional symbols
     var attribute = "Month_1";
 
-// 6.2.1 geojsonMarkerOptions > options
+// 6.2.1 EDIT, UPDATE VARIABLE > options
     //create marker options
     var options = {
         fillColor: "#ff7800",
@@ -74,34 +55,29 @@ function pointToLayer(feature, latlng){
         fillOpacity: 1,
     };
 
-// 6.2.1 IMPLEMENT POPUPS
-
-//For each feature, determine its value for the selected attribute
-
+// 6.2.1 ADD, POPUP
     var attValue = Number(feature.properties[attribute]);
-
     console.log(feature.properties, attValue);
 
-    //Give each feature's circle marker a radius based on its attribute value
     options.radius = calcPropRadius(attValue);
 
-    //create circle marker layer
     var layer = L.circleMarker(latlng, options);
 
-// 6.2.1 CHANGE POPUP CONTENTS
+// 6.2.2 EDIT, POPUP CONTENTS
 
-    //build popup content string starting with city...Example 2.1 line 24
-    var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p>";
+    var popupContent = "<p><b>Location:</b> " + 
+        feature.properties.City + "</p>";
 
-    //add formatted attribute to popup content string
     var month = attribute.split("_")[1];
-    popupContent += "<p><b>Rainfall in month " + month + ":</b> " + feature.properties[attribute] + " millimeters</p>";
+    popupContent += "<p><b>Rainfall in month " + month + ":</b> " + 
+        feature.properties[attribute] + " millimeters</p>";
 
-    //bind the popup to the circle marker
+// 6.2.4 EDIT, OFFSET MARKER
+
     layer.bindPopup(popupContent, {
         offset: new L.Point(0,-options.radius)
     });
-    //return the circle marker to the L.geoJson pointToLayer option
+
     return layer;
 };
 
@@ -113,22 +89,45 @@ function createPropSymbols(data){
     }).addTo(map);
 };
 
+// 6.3.5 ADD, SLIDER
 
-// 6.1 SET DATA TO INDIVIDUAL PROJECT
+function createSequenceControls(){
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+};
 
-//Step 2: Import GeoJSON data
-function getData(){
-    //load the data
+// 6.3.6 EDIT, SLIDER ATTRIBUTES - RETURNING NULL
+    // document.querySelector(".range-slider").max = 11;
+    // document.querySelector(".range-slider").min = 0;
+    // document.querySelector(".range-slider").value = 0;
+    // document.querySelector(".range-slider").step = 1;
+
+// 6.3.7 ADD, STEP BUTTONS
+
+    document.querySelector('#panel').
+        insertAdjacentHTML(
+        'beforeend','<button class="step" id="reverse"></button>');
+    document.querySelector('#panel').
+        insertAdjacentHTML(
+        'beforeend','<button class="step" id="forward"></button>');
+
+    document.querySelector('#reverse').
+        insertAdjacentHTML('beforeend',"<img src='img/UmbrellaLeft.png'>")
+    document.querySelector('#forward').
+        insertAdjacentHTML('beforeend',"<img src='img/UmbrellaRight.png'>")
+
+// 6.1 EDIT, SET DATA
+
+function getData(map){
     fetch("data/RainOfAsia.geojson")
         .then(function(response){
             return response.json();
         })
         .then(function(json){
-            //calculate minimum data value
             minValue = calculateMinValue(json);
-            //call function to create proportional symbols
             createPropSymbols(json);
+            createSequenceControls();
         })
 };
 
-document.addEventListener('DOMContentLoaded',createMap)// greates geoJSON layer
+document.addEventListener('DOMContentLoaded',createMap)
